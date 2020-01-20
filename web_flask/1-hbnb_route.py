138b9109572c628a0f5bc9808d74cfe3b90f0bd4
dilanrinconhbtn <937@holbertonschool.com>
2020-01-20 11:00:44 -0500

    fixing pep and task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,12 +4,19 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_world():
-    """ Message """
+    """ Message Hello """
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """ Message HBNB """
+    return 'HBNB'
 
 
 if __name__ == '__main__':
