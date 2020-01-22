a2ae0674fa9cfbbaad18da0b36e657d513bc738e
dilanrinconhbtn <937@holbertonschool.com>
2020-01-22 12:05:11 -0500

    template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Flask """
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ Message Hello """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Message HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ Message """
+    words = text.replace('_', ' ')
+    return 'C {}'.format(words)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def hello_py(text='is cool'):
+    """ Message """
+    words = text.replace('_', ' ')
+    return 'Python {}'.format(words)
+
+
+@app.route('/number/<int:number>')
+def is_number(number):
+    """ Is a Number """
+    return '%s is a number' % number
+
+
+@app.route('/number_template/<int:number>')
+def number_template(number=None):
+    """return hello hbnb!"""
+    return render_template('5-number.html', number=number)
+
+
+if __name__ == '__main__':
+    app.run()
